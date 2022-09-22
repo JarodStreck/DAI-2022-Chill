834a102da1510d20993911fcacbc48bded54c0b4
Kylian <42773865+kylianmanzini@users.noreply.github.com>
2022-09-22 11:41:30 +0200

    adding magners

---
+++ src/main/java/ch/heigvd/dai/chill/domain/kylianmanzini/Magners.java
@@ -0,0 +1,21 @@
+package ch.heigvd.dai.chill.domain.kylianmanzini;
+
+import ch.heigvd.dai.chill.domain.IProduct;
+
+import java.math.BigDecimal;
+
+public class Magners implements IProduct {
+
+    public final static String NAME = "Magners";
+    public final static BigDecimal PRICE = new BigDecimal(4.8);
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    @Override
+    public BigDecimal getPrice() {
+        return PRICE;
+    }
+}
