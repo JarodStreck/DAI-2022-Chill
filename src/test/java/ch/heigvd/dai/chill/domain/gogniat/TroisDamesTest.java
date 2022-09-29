ae056f951b8f65163f158b6b1d485bf2ae6fd88c
Michael Gogniat <michael.gogniat@heig-vd.ch>
2022-09-29 10:43:27 +0200

    test de bière

---
+++ src/test/java/ch/heigvd/dai/chill/domain/gogniat/TroisDamesTest.java
@@ -0,0 +1,31 @@
+package ch.heigvd.dai.chill.domain.gogniat;
+
+import ch.heigvd.dai.chill.domain.Bartender;
+import ch.heigvd.dai.chill.protocol.OrderRequest;
+import ch.heigvd.dai.chill.protocol.OrderResponse;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TroisDamesTest {
+
+  @Test
+  void thePriceAndNameForPunkIPAShouldBeCorrect() {
+    PunkIPA beer = new TroisDames();
+    assertEquals(beer.getName(), TroisDames.NAME);
+    assertEquals(beer.getPrice(), TroisDames.PRICE);
+  }
+
+  @Test
+  void aBartenderShouldAcceptAnOrderForTroisDames() {
+    Bartender jane = new Bartender();
+    String productName = "ch.heigvd.dai.chill.domain..gogniat.TroisDames";
+    OrderRequest request = new OrderRequest(3, productName);
+    OrderResponse response = jane.order(request);
+    BigDecimal expectedTotalPrice = PunkIPA.PRICE.multiply(new BigDecimal(3));
+    assertEquals(expectedTotalPrice, response.getTotalPrice());
+  }
+
+}
