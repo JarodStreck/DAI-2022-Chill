f19df5aa96a03a892546cd2b95662abc57d70a12
Kylian <42773865+kylianmanzini@users.noreply.github.com>
2022-09-29 10:46:22 +0200

    adding sirop

--- src/main/java/ch/heigvd/dai/chill/domain/kylianmanzini/Sirop.java
+++ src/main/java/ch/heigvd/dai/chill/domain/kylianmanzini/Sirop.java
@@ -1,13 +1,13 @@
-package ch.heigvd.dai.chill.domain.wasadigi;
+package ch.heigvd.dai.chill.domain.kylianmanzini;
 
 import ch.heigvd.dai.chill.domain.IProduct;
 
 import java.math.BigDecimal;
 
-public class PunkIPA implements IProduct {
+public class Sirop implements IProduct {
 
-  public final static String NAME = "Punk IPA";
-  public final static BigDecimal PRICE = new BigDecimal(4.2);
+  public final static String NAME = "Sirop";
+  public final static BigDecimal PRICE = new BigDecimal(2.2);
 
   @Override
   public String getName() {
